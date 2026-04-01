19c449d55699b808e32e618e3ae4aa976e656277
Fatma Agil <fatma.agil@strathmore.edu>
2026-04-01 16:16:27 +0300

    Kanban views

--- student_manager/models/course.py
+++ student_manager/models/course.py
@@ -1,15 +1,24 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'student.course'
     _description = 'Course'
 
     name = fields.Char(string="Course Name", required=True)
-    credits = fields.Integer(string="Credits")
-
 
     enrollment_ids = fields.One2many(
         'student.enrollment',
         'course_id',
         string="Enrollments"
     )
+
+    total_enrollments = fields.Integer(
+        string="Total Enrollments",
+        compute="_compute_total_enrollments",
+        store=True
+    )
+
+    @api.depends('enrollment_ids')
+    def _compute_total_enrollments(self):
+        for record in self:
+            record.total_enrollments = len(record.enrollment_ids)
